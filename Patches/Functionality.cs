6ad65083880a39228bbdd5b40332956d9a2227fc
Kevin Morris <kevr@0cost.org>
2023-12-09 08:26:49 -0800

    initial commit
    Signed-off-by: Kevin Morris <kevr@0cost.org>

---
+++ Patches/Functionality.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx.Logging;
+
+namespace StayInYourBiome.Patches;
+
+[HarmonyPatch(typeof(SpawnSystem), nameof(SpawnSystem.UpdateSpawnList))]
+static class UpdateSpawnList
+{
+    public static ManualLogSource Logger = new ManualLogSource($"{Plugin.GUID}.UpdateSpawning");
+
+    static bool Prefix(SpawnSystem __instance, List<SpawnSystem.SpawnData> spawners, DateTime currentTime, bool eventSpawners)
+    {
+        // Modify all spawners
+        foreach (SpawnSystem.SpawnData spawner in spawners)
+        {
+            string fixedKey = "defeated_kevver";
+            bool keyIsNull = string.IsNullOrEmpty(spawner.m_requiredGlobalKey);
+            bool beenFixed = spawner.m_requiredGlobalKey == fixedKey;
+            if (!keyIsNull && !beenFixed)
+            {
+                Logger.LogInfo($"Changing spawner required global key from '{spawner.m_requiredGlobalKey}' to '{fixedKey}'");
+                spawner.m_requiredGlobalKey = fixedKey;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using StayInYourBiome.Patches;
+using HarmonyLib;
+using System.Reflection;
+
+namespace StayInYourBiome
+{
+    [BepInPlugin(Plugin.GUID, Plugin.NAME, Plugin.VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string NAME = "StayInYourBiome";
+        public const string AUTHOR = "Kevver";
+        public const string GUID = $"{AUTHOR}.{NAME}";
+        public const string VERSION = "1.0.0";
+
+        private readonly Harmony harmony = new(NAME);
+
+        private void Awake()
+        {
+            // Initialize loggers
+            BepInEx.Logging.Logger.Sources.Add(UpdateSpawnList.Logger);
+
+            // Patch assembly
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            harmony.PatchAll(assembly);
+        }
+    }
+}
